7779512569061bffb4711af49c587fb27803a726
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-03-11 16:34:44 -0300

    Conexão com o banco de dados realizado, consulta funcionando(arrumar a inserção)

--- Controller/ConexaoComBanco.ts
+++ Controller/ConexaoComBanco.ts
@@ -1,30 +1,83 @@
-import { Console, error } from "console";
 
 
-const pg = require("pg");
 const { Pool } = require('pg');
 
 class Banco{
     dbConfig = {
-        user: 'seu_usuario',
-        host: 'seu_host',
-        database: 'seu_banco_de_dados',
-        password: 'sua_senha',
-        port: "seu_porta",
+        user: 'avnadmin',
+        host: 'bancodedadosdopii-maua-b52c.a.aivencloud.com',
+        database: 'defaultdb',
+        password: 'AVNS_62DP2ALu9x2P5u0YPwu',
+        port: "11379",
         ssl: {
           rejectUnauthorized: false, // Necessário para conexão com Aiven
         },
       };
     pool = new Pool(this.dbConfig)
 
-    verTabelas() {
+
+    async verTabelaUsuario() {
         try{
-            const resultado = this.pool.query('SELECT * FROM tabela')
-            console.log(resultado)
+            const resultado =  await this.pool.query('SELECT * FROM usuario')
+            for (let rows of resultado.rows){
+                console.log(rows)
+            }
          
         }
         catch{
-            console.error("Deu erro!")
+            console.log("Erro")
         }
     }
+    async testarConexao(){
+        try{
+            await this.pool
+            console.log("Conexão funcionando!")
+        }
+         
+        catch{
+            console.log("Conexão não efeituada")
+        }
+
+    }
+
+
+    async validarLogin(username,password){
+        let validacao : string[] = [];
+        let validacaoDoUser : string[] = []
+        let validacaoDoPassword
+        let passou
+        const resultado =  await this.pool.query('SELECT user, password FROM usuario WHERE user = ' + username)
+            for (let res of resultado.res){
+                validacao.push(res)
+            }
+            for(const[user,password] of validacao){
+                validacaoDoUser.push(user)
+                validacaoDoPassword.push(password)
+            }
+            if(validacaoDoPassword[0] == password){
+                console.log("Bem-Vindo de volta " + username)
+                passou = true
+            }else{
+                console.log("Senha Incorreta")
+            }   
+        validacaoDoUser = []
+        validacaoDoPassword = []
+    }
+
+
+    async cadastrarNovoUsuario(userr: string,password: string){
+        try{
+            const values = [userr,password]
+            const query = `INSERT INTO usuario ("user", "password") VALUES (${1},${2})`
+            await this.pool.query({text: query,values})
+            console.log("Cadastrado!")
+        }catch(error){
+            console.error(error.message)
+        }
+
+    }
+
 }
+const x = new Banco();
+x.testarConexao()
+x.cadastrarNovoUsuario("Victor123456","123456")
